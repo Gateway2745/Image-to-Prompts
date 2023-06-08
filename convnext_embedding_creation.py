8c8f53337edc323ed06bf41bb49ed59bdf4684a0
Sai Sree Harsha <sreesai1412@gmail.com>
2023-06-07 19:01:45 -0700

    Update convnext_embedding_creation.py

--- convnext_embedding_creation.py
+++ convnext_embedding_creation.py
@@ -5,39 +5,31 @@ from tqdm import tqdm
 from PIL import Image
 from transformers import AutoImageProcessor, ConvNextModel
 
-def create_convnext_embeddings():
+def create_convnext_embeddings(folder_path, batch_size):
     device = "cuda"
     image_processor = AutoImageProcessor.from_pretrained("facebook/convnext-tiny-224")
     model = ConvNextModel.from_pretrained("facebook/convnext-tiny-224").to(device=device)
 
-    for j in range(1, 51):
-        image_paths = sorted(glob.glob("./image_hierarchy/part-" + str(j) + "/*.png"))
+    image_paths = sorted(glob.glob(folder_path))
 
-        image_embeddings = []
+    image_embeddings = []
+    n_images = len(image_paths)
+    for batch_idx in tqdm(range(0, n_images, batch_size)):
+        batch_images = []
+        for i in range(batch_idx, min(n_images, batch_idx+batch_size)):
+            image = image_processor(Image.open(image_paths[i]), return_tensors="pt")['pixel_values']
+            batch_images.append(image)
+    
+        image_batch = torch.cat(batch_images, dim=0).to(device)
+        with torch.no_grad():
+            image_features = model(image_batch).pooler_output.cpu().numpy()
+            image_embeddings.append(image_features)
+        
+        
+    image_embeddings = np.concatenate(image_embeddings, axis=0)
+    return image_embeddings
 
-        n_images = len(image_paths)
-        for i in tqdm(range(n_images)):
-            image = Image.open(image_paths[i])            
-            image_batch = image_processor(image, return_tensors="pt")['pixel_values'].to(device)
-            with torch.no_grad():
-                image_features = model(image_batch).pooler_output.cpu().numpy()
-                image_embeddings.append(image_features)
-            del image_batch
-
-        paths = [p.split("/")[-1] for p in image_paths]
-        embedding_dict = dict(zip(paths, image_embeddings))
-        np.save("convnext-embeddings/convnext-part-" + str(j)+ ".npy", embedding_dict)
     
 if __name__ == "__main__":
-    create_convnext_embeddings()
-
-    filenames = glob.glob("./convnext-embeddings/*.npy")
-
-    dfs = {}
-    for i, filename in enumerate(filenames):
-        d = np.load(filename, allow_pickle=True).item()
-        dfs.update(d)
-
-    sorted_dict = dict(sorted(dfs.items()))
-    image_embeddings = np.concatenate(list(sorted_dict.values()), axis=0)
+    create_convnext_embeddings("./images/*.png", batch_size=50)
     np.save('50k_convnext_embeddings.npy', image_embeddings)
