fdd4fc9658ff89faaf675705347a39abbea97dc2
Sai Sree Harsha <sreesai1412@gmail.com>
2023-06-07 19:00:55 -0700

    Update clip_intgtr_embedding_creation.py

--- clip_intgtr_embedding_creation.py
+++ clip_intgtr_embedding_creation.py
@@ -1,33 +1,56 @@
-import torch
-import clip
+import os
+import csv
 import glob
 import numpy as np
-from tqdm import tqdm
+import pandas as pd
+
 from PIL import Image
+from tqdm import tqdm
 
-def create_clip_embeddings(folder_path, batch_size):
-    model, preprocess = clip.load("ViT-B/32", device="cuda")
+import os, subprocess
+from IPython.display import clear_output, display
+from clip_interrogator import Config, Interrogator
+from sentence_transformers import SentenceTransformer, models
 
-    image_paths = sorted(glob.glob(folder_path))
+def image_to_prompt(ci, image, mode):
+    ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    ci.config.flavor_intermediate_count = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    image = image.convert('RGB')
+    if mode == 'best':
+        return ci.interrogate(image)
+    elif mode == 'classic':
+        return ci.interrogate_classic(image)
+    elif mode == 'fast':
+        return ci.interrogate_fast(image)
+    elif mode == 'negative':
+        return ci.interrogate_negative(image)
 
-    image_embeddings = []
+def create_clip_intgtr_embeddings(folder_path):
+    caption_model_name = 'blip-large' 
+    clip_model_name = 'ViT-H-14/laion2b_s32b_b79k'
 
-    n_images = len(image_paths)
-    for batch_idx in tqdm(range(0, n_images, batch_size)):
-        batch_images = []
-        for i in range(batch_idx,min(n_images, batch_idx + batch_size)):
-            image = preprocess(Image.open(image_paths[i])).unsqueeze(0)
-            batch_images.append(image)
-            
-        image_batch = torch.cat(batch_images, dim=0).to("cuda")
-        with torch.no_grad():
-            image_features = model.encode_image(image_batch).cpu().numpy()
-            image_embeddings.append(image_features)
-        
-    image_embeddings = np.concatenate(image_embeddings, axis=0)
+    config = Config()
+    config.clip_model_name = clip_model_name
+    config.caption_model_name = caption_model_name
+    ci = Interrogator(config)
 
-    return image_embeddings
-    
+    prompt_mode = 'fast'
+    ci.config.quiet = True
+
+
+    files = sorted(glob.glob(folder_path))
+    prompts = []
+    for idx, f in enumerate(tqdm(files)):
+        if idx > 0 and idx % 100 == 0:
+            clear_output(wait=True)
+        image = Image.open(f).convert('RGB')
+        prompt = image_to_prompt(ci, image, prompt_mode)
+        prompts.append(prompt)
+
+    st_model = SentenceTransformer('all-MiniLM-L6-v2', device='cuda')
+    caption_embeddings = st_model.encode(prompts, batch_size=1, convert_to_numpy=True, device='cuda')
+    return caption_embeddings
+
 if __name__ == "__main__":
-    image_embeddings = create_clip_embeddings("./images/*.png", batch_size=50)
-    np.save('img_embeddings_clip_vit32.npy', image_embeddings)
+    caption_embeddings = create_clip_intgtr_embeddings("./images/*.png")
+    np.save('50k_clip_intgtr_embeddings.npy', caption_embeddings)
