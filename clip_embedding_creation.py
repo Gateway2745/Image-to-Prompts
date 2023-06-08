beaf21c9b6d87fec9f371f6ffa7db9170bdc39da
Sai Sree Harsha <sreesai1412@gmail.com>
2023-06-07 18:59:40 -0700

    Update clip_embedding_creation.py

--- clip_embedding_creation.py
+++ clip_embedding_creation.py
@@ -5,36 +5,29 @@ import numpy as np
 from tqdm import tqdm
 from PIL import Image
 
-def create_clip_embeddings():
-    device = "cuda"
-    model, preprocess = clip.load("ViT-B/32", device=device)
+def create_clip_embeddings(folder_path, batch_size):
+    model, preprocess = clip.load("ViT-B/32", device="cuda")
 
-    for j in range(1, 51):
-        image_paths = sorted(glob.glob("./image_hierarchy/part-" + str(j) + "/*.png"))
-        image_embeddings = []
+    image_paths = sorted(glob.glob(folder_path))
 
-        n_images = len(image_paths)
-        for i in tqdm(range(n_images)):            
-            image_batch = preprocess(Image.open(image_paths[i])).unsqueeze(0).to(device)
-            with torch.no_grad():
-                image_features = model.encode_image(image_batch).cpu().numpy()
-                image_embeddings.append(image_features)
-            del image_batch
+    image_embeddings = []
+
+    n_images = len(image_paths)
+    for batch_idx in tqdm(range(0, n_images, batch_size)):
+        batch_images = []
+        for i in range(batch_idx,min(n_images, batch_idx + batch_size)):
+            image = preprocess(Image.open(image_paths[i])).unsqueeze(0)
+            batch_images.append(image)
+            
+        image_batch = torch.cat(batch_images, dim=0).to("cuda")
+        with torch.no_grad():
+            image_features = model.encode_image(image_batch).cpu().numpy()
+            image_embeddings.append(image_features)
         
-        paths = [p.split("/")[-1] for p in image_paths]
-        embedding_dict = dict(zip(paths, image_embeddings))
-        np.save("clip-embeddings/clip-part-" + str(j)+ ".npy", embedding_dict)
+    image_embeddings = np.concatenate(image_embeddings, axis=0)
+
+    return image_embeddings
     
 if __name__ == "__main__":
-    create_clip_embeddings()
-
-    filenames = glob.glob("./clip-embeddings/*.npy")
-
-    dfs = {}
-    for i, filename in enumerate(filenames):
-        d = np.load(filename, allow_pickle=True).item()
-        dfs.update(d)
-
-    sorted_dict = dict(sorted(dfs.items()))
-    image_embeddings = np.concatenate(list(sorted_dict.values()), axis=0)
+    image_embeddings = create_clip_embeddings("./images/*.png", batch_size=50)
     np.save('50k_clip_embeddings.npy', image_embeddings)
